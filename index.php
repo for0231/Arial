ed4b1ff6b84af5261f91e72538cc4b03bf161610
shawn <for0231@gmail.com>
2013-01-06 10:47:59 +0800

    fix

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * 处理请求的主入口
+ * 
+ * 所有Arial代码都基于GNU通用公共许可证发布。
+ */
+
+/**
+ * Arial 请求主入口
+ */
+define ('ARIAL_ROOT', getcwd());
+
+//require_once ARIAL_ROOT . '/includes/bootstrap.inc';
+echo ARIAL_ROOT;
+echo "<br />这里是Arial系统请求的主入口";
